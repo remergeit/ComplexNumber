93fa93263729b16266d08093d4977ef435f126ab
Alex <remergeit@gmail.com>
2020-10-07 21:14:17 +0300

    added polar form

--- src/ComplexNumber.php
+++ src/ComplexNumber.php
@@ -26,8 +26,65 @@ class ComplexNumber
 
     public function __construct($real = 0.0, $imaginary = 0.0)
     {
-        $this->real = (float) $real;
-        $this->imaginary = (float) $imaginary;
+        if (func_num_args() === 1 && is_string(func_get_arg(0))) {
+            $rectangularRegex =
+                // Match any float, negative or positive, its Real part, group 1
+                '/^\s*([-+]?\d+|[-+]?\d+\.?\d+)' .
+                // ... possibly following that with whitespaces
+                '\s*' .
+                // Match any other float followed by 'i', its Imaginary part, group 2
+                '([-+]?\s*\d+|[-+]?\s*\d+\.?\d+)\s*[i]\s*$/';
+            $rectangularSuccess = preg_match($rectangularRegex, func_get_arg(0), $rectangularMatch);
+            if ($rectangularSuccess) {
+                $this->real = (float) $rectangularMatch[1];
+                $this->imaginary = (float) $rectangularMatch[2];
+                return;
+            }
+            $polarRegex =
+                // Match only positive float, its Modulus, group 1
+                '/^\s*([+]?\d+|[+]?\d+\.?\d+)' .
+                // ... possibly following that with whitespaces or ','
+                '\s*[,]?\s+' .
+                // Match other only positive float, possibly followed by '°', its Degree, group 2
+                '([+]?\d+|[+]?\d+\.?\d+)\s*[\xB0|\x{00B0}]?\s*$/u';
+            $polarSuccess = preg_match($polarRegex, func_get_arg(0), $polarMatch);
+            if ($polarSuccess) {
+                $modulus = (float) $polarMatch[1];
+                $degree = (float) $polarMatch[2];
+                $this->real = $modulus * cos(deg2rad($degree));
+                $this->imaginary = $modulus * sin(deg2rad($degree));
+                return;
+            }
+            $polarHumanRegex =
+                // Match only positive float, its Modulus, group 1
+                '/^\s*([+]?\d+|[+]?\d+\.?\d+)' .
+                // ... possibly following that with whitespaces, '*' or 'x' before first round bracket
+                '\s*[*x]?\s*[(]\s*' .
+                // Match first 'cos' or 'i sin' inside round brackets
+                '(?:(?:(?:(cos)|i\s*[*x]?\s*(sin))\s*' .
+                // Match other only positive float, possibly followed by '°' and in round brackets, its Degree, group 4
+                '[(]?\s*([+]?\d+|[+]?\d+\.?\d+)\s*[\xB0|\x{00B0}]?\s*[)]?)' .
+                // ... following that with '+'
+                '\s*[+]\s*' .
+                // Match second 'cos' or 'i sin' inside round brackets, should not be the same as first
+                '(?:(?:(?!\2)(cos)|i\s*[*x]?\s*(?!\3)(sin))\s*' .
+                // Match Degree one more time, should be the same as first
+                '[(]?\s*(\4)\s*[\xB0|\x{00B0}]?\s*[)]?))' .
+                // ... possibly following that with whitespaces before and after last round bracket
+                '\s*[)]\s*$/u';
+            $polarHumanSuccess = preg_match($polarHumanRegex, func_get_arg(0), $polarHumanMatch);
+            if ($polarHumanSuccess) {
+                $modulus = (float) $polarHumanMatch[1];
+                $degree = (float) $polarHumanMatch[4];
+                $this->real = $modulus * cos(deg2rad($degree));
+                $this->imaginary = $modulus * sin(deg2rad($degree));
+                return;
+            }
+            throw new \InvalidArgumentException('Parse string failed');
+        } else {
+            $this->real = (float) $real;
+            $this->imaginary = (float) $imaginary;
+        }
     }
 
     public function __toString(): string
@@ -57,6 +114,75 @@ class ComplexNumber
     public function getImaginary(): float
     {
         return $this->imaginary;
+    }
+
+    /**
+     * Gets the modulus of this complex number
+     *
+     * @return float
+     */
+    public function getModulus(): float
+    {
+        return sqrt($this->real ** 2 + $this->imaginary ** 2);
+    }
+
+    /**
+     * Gets the degree of this complex number
+     *
+     * @return float
+     */
+    public function getDegree(): float
+    {
+        // tangent undefined at 90, 270 degrees
+        if ($this->real === 0.0) {
+            $degree = $this->imaginary > 0
+                      ? 90
+                      : ($this->imaginary < 0
+                         ? 270
+                         : 0);
+            return $degree;
+        } else {
+            $tangent = abs($this->imaginary) / abs($this->real);
+            $degree = rad2deg(atan($tangent));
+
+            $quadrant1 = $this->real > 0 && $this->imaginary >= 0;
+            $quadrant2 = $this->real < 0 && $this->imaginary >= 0;
+            $quadrant3 = $this->real < 0 && $this->imaginary <= 0;
+            $quadrant4 = $this->real > 0 && $this->imaginary <= 0;
+            if ($quadrant1) {
+                return $degree;
+            }
+            if ($quadrant2) {
+                return 180 - $degree;
+            }
+            if ($quadrant3) {
+                return 180 + $degree;
+            }
+            if ($quadrant4) {
+                return 360 - $degree;
+            }
+        }
+    }
+
+    /**
+     * Returns the formatted string representation of this complex number
+     *
+     * @param string $format
+     * @return string
+     */
+    public function format(string $format): string
+    {
+        switch ($format) {
+            case 'rectangular':
+                return (string) $this;
+            case 'polar':
+                return $this->getModulus() . ', ' . $this->getDegree();
+            case 'polarHuman':
+                $degree = $this->getDegree();
+                return $this->getModulus() . "(cos $degree ° + i sin $degree °)";
+            default:
+                return (string) $this;
+        }
     }
 
     /**
--- tests/unit/ComplexNumberTest.php
+++ tests/unit/ComplexNumberTest.php
@@ -40,6 +40,72 @@ class ComplexNumberTest extends TestCase
     /**
      * @return array[]
      */
+    public function providerInstantiateWithString(): array
+    {
+        $oneComplexNumberStringDataSets = array_chunk(array_merge(...$this->providerFormat()), 1);
+        $oneComplexNumberValueDataSets = $this->arrayUniqueMultidimensional($this->providerInstantiateWithArguments());
+        $expectedResults = [];
+        foreach ($oneComplexNumberValueDataSets as $oneComplexNumberValueDataSet) {
+            $expectedResults = array_merge(
+                $expectedResults,
+                array_fill(0, 3, $oneComplexNumberValueDataSet)
+            );
+        }
+
+        return array_map('array_merge', $oneComplexNumberStringDataSets, $expectedResults);
+    }
+
+    /**
+     * @return array[]
+     */
+    public function providerInstantiateWithStringStress(): array
+    {
+        return [
+            ['123 + 0i', 123, 0],
+            ['123 0', 123, 0],
+            ['123 (cos 0 ° + i sin 0 °)', 123, 0],
+            ['456 -0i', 456, 0],
+            ['456, 0°', 456, 0],
+            ['456 *  (cos 0 ° + i   sin 0 °)', 456, 0],
+            ['123.456+0i', 123.456, 0],
+            ['123.456, 0', 123.456, 0],
+            ['123.456  x(cos (0 ) + i *sin ( 0 °  ))', 123.456, 0],
+            ['789.012+0i', 789.012, 0],
+            ['789.012, 0', 789.012, 0],
+            ['789.012x  (i sin 0 ° + cos 0 °)', 789.012, 0],
+            ['123.456+78.9i', 123.456, 78.9],
+            ['146.51482497004, 32.582405557983', 123.456, 78.9],
+            ['146.51482497004   (cos 32.582405557983   + i x sin 32.582405557983 )', 123.456, 78.9],
+        ];
+    }
+
+    /**
+     * @return array[]
+     */
+    public function providerInstantiateWithStringInvalid(): array
+    {
+        return [
+            ['123 + 0'],
+            ['123. 0'],
+            ['123 (cos 0 ° + sin 0 °)'],
+            ['456 -i0'],
+            ['456,0°'],
+            ['456 *  (cos 0 ° + cos 0 °)'],
+            ['123,456+0i'],
+            ['123,456 0'],
+            ['123.456  X(cos (0 ) + i *sin ( 0 °  ))'],
+            ['789.012+0ii'],
+            ['789.012, -10'],
+            ['789.012x  (i sin -10 ° + cos -10 °)'],
+            ['123.456+78,9i'],
+            ['146.51482497004, 32.582405557983 deg'],
+            ['146.51482497004   (cos 32.582   + i x sin 32.582405557983 )'],
+        ];
+    }
+
+    /**
+     * @return array[]
+     */
     public function providerToString(): array
     {
         $expectedResults = [
@@ -53,11 +119,27 @@ class ComplexNumberTest extends TestCase
             ['0+1i'],
             ['0-1i'],
         ];
-        $serialized = array_map('serialize', $this->providerInstantiateWithArguments());
-        $unique = array_unique($serialized);
-        $oneComplexNumberValueDataSets = array_intersect_key($this->providerInstantiateWithArguments(), $unique);
+        $oneComplexNumberValueDataSets = $this->arrayUniqueMultidimensional($this->providerInstantiateWithArguments());
 
         return array_map('array_merge', $oneComplexNumberValueDataSets, $expectedResults);
+    }
+
+    /**
+     * @return array[]
+     */
+    public function providerFormat(): array
+    {
+        return [
+            ['123+0i', '123, 0', '123(cos 0 ° + i sin 0 °)'],
+            ['456+0i', '456, 0', '456(cos 0 ° + i sin 0 °)'],
+            ['123.456+0i', '123.456, 0', '123.456(cos 0 ° + i sin 0 °)'],
+            ['789.012+0i', '789.012, 0', '789.012(cos 0 ° + i sin 0 °)'],
+            ['123.456+78.9i', '146.51482497004, 32.582405557983', '146.51482497004(cos 32.582405557983 ° + i sin 32.582405557983 °)'],
+            ['-987.654-32.1i', '988.17550754712, 181.86152977919', '988.17550754712(cos 181.86152977919 ° + i sin 181.86152977919 °)'],
+            ['-987.654+0i', '987.654, 180', '987.654(cos 180 ° + i sin 180 °)'],
+            ['0+1i', '1, 90', '1(cos 90 ° + i sin 90 °)'],
+            ['0-1i', '1, 270', '1(cos 270 ° + i sin 270 °)'],
+        ];
     }
 
     /**
@@ -178,17 +260,51 @@ class ComplexNumberTest extends TestCase
     }
 
     /**
+     * @dataProvider providerInstantiateWithString
+     * @param string $string
+     * @param mixed $real
+     * @param mixed $imaginary
      * @return void
      */
-    public function testInvalidComplexNumber(): void
+    public function testInstantiateWithString(string $string, $real, $imaginary): void
     {
-        $complexNumber = new ComplexNumber('ABCDE');
+        $complexNumber = new ComplexNumber($string);
 
         $complexNumberReal = $complexNumber->getReal();
-        $this->assertEquals(0.0, $complexNumberReal);
+        $this->assertEquals($real ?? 0.0, $complexNumberReal);
 
         $complexNumberImaginary = $complexNumber->getImaginary();
-        $this->assertEquals(0.0, $complexNumberImaginary);
+        $this->assertEquals($imaginary ?? 0.0, $complexNumberImaginary);
+    }
+
+    /**
+     * @dataProvider providerInstantiateWithStringStress
+     * @param string $string
+     * @param mixed $real
+     * @param mixed $imaginary
+     * @return void
+     */
+    public function testInstantiateWithStringStress(string $string, $real, $imaginary): void
+    {
+        $complexNumber = new ComplexNumber($string);
+
+        $complexNumberReal = $complexNumber->getReal();
+        $this->assertEquals($real ?? 0.0, $complexNumberReal);
+
+        $complexNumberImaginary = $complexNumber->getImaginary();
+        $this->assertEquals($imaginary ?? 0.0, $complexNumberImaginary);
+    }
+
+    /**
+     * @dataProvider providerInstantiateWithStringInvalid
+     * @param string $stringInvalid
+     * @return void
+     */
+    public function testInvalidComplexNumber(string $stringInvalid): void
+    {
+        $this->expectException(\InvalidArgumentException::class);
+
+        $complexNumber = new ComplexNumber($stringInvalid);
     }
 
     /**
@@ -204,6 +320,27 @@ class ComplexNumberTest extends TestCase
 
         $complexNumberAsString = (string)$complexNumber;
         $this->assertEquals($expected, $complexNumberAsString);
+    }
+
+    /**
+     * @dataProvider providerFormat
+     * @param string $rectangular
+     * @param string $polar
+     * @param string $polarHuman
+     * @return void
+     */
+    public function testFormat(string $rectangular, string $polar, string $polarHuman): void
+    {
+        $complexNumber = new ComplexNumber($rectangular);
+
+        $complexNumberFormatRectangular = $complexNumber->format('rectangular');
+        $this->assertEquals($rectangular, $complexNumberFormatRectangular);
+
+        $complexNumberFormatPolar = $complexNumber->format('polar');
+        $this->assertEquals($polar, $complexNumberFormatPolar);
+
+        $complexNumberFormatPolarHuman = $complexNumber->format('polarHuman');
+        $this->assertEquals($polarHuman, $complexNumberFormatPolarHuman);
     }
 
     /**
@@ -339,5 +476,18 @@ class ComplexNumberTest extends TestCase
 
         $firstComplexNumber = new ComplexNumber(123, null);
         $firstComplexNumber->divide(new ComplexNumber());
+    }
+
+    /**
+     * Remove duplicate items from a multidimensional array
+     *
+     * @param array $multidimensionalDuplicates
+     * @return array[]
+     */
+    private function arrayUniqueMultidimensional(array $multidimensionalDuplicates): array
+    {
+        $serialized = array_map('serialize', $multidimensionalDuplicates);
+        $unique = array_unique($serialized);
+        return array_intersect_key($multidimensionalDuplicates, $unique);
     }
 }
